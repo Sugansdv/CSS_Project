24c35bce1e82c733418506887d089b86949f9a33
Sugansdv <suganyasdv16@gmail.com>
2025-03-27 20:56:20 +0530

    add

---
+++ Javascript/Day 3/script1.js
@@ -0,0 +1,13 @@
+
+let Username = "sugansdv";
+let Password = "sugan21";
+
+let username = prompt("Enter your username:");
+let password = prompt("Enter your password:");
+
+
+if (username === Username && password === Password) {
+    console.log("✅ Login successful!");
+} else {
+    console.log("❌ Invalid credentials!");
+}
