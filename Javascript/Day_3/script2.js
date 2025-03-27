0be61bfc7bb0d5cbe01f9a20885541e9ddd3127e
Sugansdv <suganyasdv16@gmail.com>
2025-03-27 21:30:05 +0530

    Added new folder

---
+++ Javascript/Day_3/script2.js
@@ -0,0 +1,11 @@
+let color = prompt("🚦Enter a traffic light color (red, yellow, green):");
+
+if (color === "red") {
+    console.log("🛑 Stop!");
+} else if (color === "yellow") {
+    console.log("⚠️ Slow down!");
+} else if (color === "green") {
+    console.log("✅ Go!");
+} else {
+    console.log("❌ Invalid color!");
+}
